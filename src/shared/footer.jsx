4bba5668a980fa34d53c7a0d2f979325cab37266
cacquani <cacquani@mail.com>
2018-12-12 08:37:08 +0000

    ESlint and fixes

--- src/shared/footer.jsx
+++ src/shared/footer.jsx
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
@@ -9,23 +9,23 @@ class Header extends Component {
         </div>
         <div className="half">
           <a className="mail" href="mailto:cacquani@mail.com">
-            <i className="far fa-2x fa-envelope"></i>
+            <i className="far fa-2x fa-envelope" />
             <span>Email</span>
           </a>
           <a className="whatsapp" href="whatsap://+44 7468 923163">
-            <i className="fab fa-2x fa-whatsapp"></i>
+            <i className="fab fa-2x fa-whatsapp" />
             <span>Whatsapp</span>
           </a>
           <a className="linkedin" href="https://www.linkedin.com/in/cristina-acquani-3a613226/">
-            <i className="fab fa-2x fa-linkedin-in"></i>
+            <i className="fab fa-2x fa-linkedin-in" />
             <span>Linkedin</span>
           </a>
           <a className="gitlab" href="https://gitlab.com/cacquani">
-            <i className="fab fa-2x fa-gitlab"></i>
+            <i className="fab fa-2x fa-gitlab" />
             <span>GitLab</span>
           </a>
           <a className="github" href="https://github.com/cacquani">
-            <i className="fab fa-2x fa-github"></i>
+            <i className="fab fa-2x fa-github" />
             <span>GitHub</span>
           </a>
         </div>
