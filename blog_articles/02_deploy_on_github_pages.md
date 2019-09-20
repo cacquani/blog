df6e1e06a152033414dabade5de540e6b42ab627
cacquani <cacquani@mail.com>
2019-09-20 18:38:30 +0100

    Upgrade bundles and align personal site to business cards

---
+++ blog_articles/02_deploy_on_github_pages.md
@@ -0,0 +1,8 @@
+# Deploy on Github pages
+
+Have an `index.html` and nothing else on your root folder
+
+https://help.github.com/en/categories/github-pages-basics
+
+https://help.github.com/en/articles/creating-project-pages-using-the-command-line
+
--- site/src/components/footer.jsx
+++ site/src/components/footer.jsx
@@ -7,25 +7,21 @@ class Header extends Component {
         <div className="half">
           <p>Currently based in Leeds, available for Yorkshire based or remote roles</p>
         </div>
-        <div className="half">
+        <div className="half right">
           <a className="mail" href="mailto:cacquani@mail.com">
-            <i className="far fa-2x fa-envelope" />
+            <i className="far fa-1x fa-envelope" />
             <span>Email</span>
           </a>
-          <a className="whatsapp" href="whatsap://+44 7468 923163">
-            <i className="fab fa-2x fa-whatsapp" />
-            <span>Whatsapp</span>
-          </a>
           <a className="linkedin" href="https://www.linkedin.com/in/cristina-acquani-3a613226/">
-            <i className="fab fa-2x fa-linkedin-in" />
+            <i className="fab fa-1x fa-linkedin-in" />
             <span>Linkedin</span>
           </a>
           <a className="gitlab" href="https://gitlab.com/cacquani">
-            <i className="fab fa-2x fa-gitlab" />
+            <i className="fab fa-1x fa-gitlab" />
             <span>GitLab</span>
           </a>
           <a className="github" href="https://github.com/cacquani">
-            <i className="fab fa-2x fa-github" />
+            <i className="fab fa-1x fa-github" />
             <span>GitHub</span>
           </a>
         </div>
